dd89ffd06c2c99fc663a990d21d7cf059ae0d3f6
Gustavo Costa <126887704+GustavoMiec@users.noreply.github.com>
2024-10-17 22:53:55 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
@@ -1,2 +1,28 @@
-package br.com.fiap.gitdash.config;public class SecurityConfig {
+package br.com.fiap.epictaskz.config;
+
+import br.com.fiap.epictaskz.user.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, UserService userService) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .userInfoEndpoint(userInfo -> userInfo.userService(userService))
+                        .permitAll()
+                )
+                //.formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+
 }
